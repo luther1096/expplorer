8a93dd5fcbb0cbc8070e8674568b89c12efc9130
luther <luther1096@gmail.com>
2019-11-29 01:09:52 +0530

    add place form added

--- Frontend/src/places/pages/NewPlace.js
+++ Frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,21 @@
 import React from "react";
+import "./NewPlace.css";
+import Input from "../../shared/components/FormElements/Input";
+
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 const NewPlace = () => {
-  return <h1>Places works!!!</h1>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
