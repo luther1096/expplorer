c42d9029614d4a1b4183d8bd6689fbf11b1f25a2
luther <luther1096@gmail.com>
2019-11-29 20:27:39 +0530

    places route and user-places route implemented

---
+++ Backend/routes/places-routes.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+// const HttpError = require("../models/http-error");
+const placesController = require("../controllers/places-controller");
+
+const router = express.Router();
+
+// const DUMMY_PLACES = [
+//   {
+//     id: "p1",
+//     title: "Empire State Building",
+//     description: "One of the most famous sky scrapers in the World!",
+//     location: {
+//       lat: 40.7484474,
+//       lng: -73.9871516
+//     },
+//     address: "20 W 34th St, New York, NY 10001",
+//     creator: "u1"
+//   }
+// ];
+
+// router.get("/:pid", (req, res, next) => {
+//   //   console.log("GET request received!");
+//   const placeId = req.params.pid;
+//   //   console.log(req.params);
+//   const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+//   //   if (!place) {
+//   //     return res.status(404).json({
+//   //       message: "Could not find a place for the provided id."
+//   //     });
+//   //   }
+
+//   //   if (!place) {
+//   //     const error = new Error("Could not find a place for the provided id.");
+//   //     error.code = 404;
+//   //     throw error;
+//   //   }
+
+//   if (!place) {
+//     throw new HttpError("Could not find a place for the provided id.", 404);
+//   }
+
+//   res.json({
+//     place
+//   });
+// });
+
+router.get("/places/:pid", placesController.getPlaceById);
+
+router.get("/user/:uid", placesController.getPlaceByUserId);
+
+module.exports = router;
